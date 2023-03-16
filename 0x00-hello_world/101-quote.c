f52c45e3af8b9ebb96b94c23d8e0c99b910ae2a3
HoussamMrabet <dodslim100@gmail.com>
2023-03-16 09:32:00 -0700

    error from atlantis

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -10,7 +10,8 @@
 
 int main(void)
 {
-	char msg[]= "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
 	write(1, msg, 59);
 	return (1);
 }
