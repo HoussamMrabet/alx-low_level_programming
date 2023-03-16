0322eb643dc1534b5e00288c73dea9c967c5ee86
HoussamMrabet <dodslim100@gmail.com>
2023-03-16 09:36:04 -0700

    task completed ✅✅✅

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,12 +5,12 @@
  *
  * Description : A C program that prints without printf or puts
  *
- * Return : Always 1
+ * Return : return an error 1
 */
 
 int main(void)
 {
-	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(1, msg, 59);
 	return (1);
