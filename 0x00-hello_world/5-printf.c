4978b65cc373b55ff0a2d80ac5ad3ac193e9747c
HoussamMrabet <dodslim100@gmail.com>
2023-03-16 08:02:40 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description:a C program that print using printf
+ * return: 0;
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
