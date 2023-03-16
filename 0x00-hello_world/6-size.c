b9d306fcef0ae82529c4cfae48d9f93b5e311c29
HoussamMrabet <dodslim100@gmail.com>
2023-03-16 08:35:23 -0700

    updatin sizeof task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,17 +3,19 @@
 /**
  * main - Entry point
  *
- * Description : A C program that prints the size of various types
+ * Description: A C program that prints the size of various types
  *
- * Return : Always 0
+ * Return: Always 0
 */
 
 int main(void)
 {
+	/* printing size of datatypes */
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: 4 byte(s)\n", sizeof(int));
 	printf("Size of a long int: 4 byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: 8 byte(s)\n", sizeof(long long int));
 	printf("Size of a float: 4 byte(s)\n", sizeof(float));
+	
 	return (0);
 }
