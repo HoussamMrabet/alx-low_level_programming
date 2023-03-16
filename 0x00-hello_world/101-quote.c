d03528a2b2a98afb475c9e195724766979506b5f
HoussamMrabet <dodslim100@gmail.com>
2023-03-16 09:28:53 -0700

    missing ;

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-	char msg[]= "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+	char msg[]= "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	write(1, msg, 59);
 	return (1);
 }
