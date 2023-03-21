61c70f5435d5a18c38ab95833f64c2ddf343d009
HoussamMrabet <dodslim100@gmail.com>
2023-03-21 14:56:47 -0700

    13. Nature made the natural numbers; All else is the work of women

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,9 +3,9 @@
 
 /**
  * main - Entry
- * Description: a program that computes and prints the sum of all the multiples of 3 or 5 below 1024
+ * Description: a program prints the sum of the multiples of 3 or 5 below 1024
  * Return: Always 0
-*/ 
+*/
 
 int main(void)
 {
