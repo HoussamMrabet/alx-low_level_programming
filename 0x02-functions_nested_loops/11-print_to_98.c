49b312ed4c0a6288d74da10f6c950deb35111a89
HoussamMrabet <dodslim100@gmail.com>
2023-03-21 12:49:57 -0700

    11. 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - Entry
+ * Description: A function that prints all natural numbers from n to 98
+ * @n: An integer
+ * Return: void
+*/
+
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+		printf("%d", i)
+}
