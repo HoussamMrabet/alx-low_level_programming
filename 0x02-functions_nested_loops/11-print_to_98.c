750dcf67eb115d1d17578e0d3c31a719f8ecc7d0
HoussamMrabet <dodslim100@gmail.com>
2023-03-21 13:08:52 -0700

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,6 +12,20 @@ void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i <= 98; i++)
-		printf("%d", i);
+	printf("%d", n);
+	if (n <= 98)
+	{
+		for (i = n + 1; i <= 98; i++)
+		{
+			printf(", %d", i);
+		}
+	}
+	else
+	{
+		for (i = n - 1; i >= 98; i--)
+		{
+			printf(", %d", i);
+		}
+	}
+	printf("\n");
 }
