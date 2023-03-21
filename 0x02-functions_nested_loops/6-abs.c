6624031a98f6f15c59bc6fa16d7dcb539c211e01
HoussamMrabet <dodslim100@gmail.com>
2023-03-21 07:13:11 -0700

    update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,7 +4,7 @@
  * _abs - Entry
  * Description: a function that computes the absolute value of an integer
  * @x: an integer
- * Returns: absolute valur of @x
+ * Returns: returns absolute value of @x
 */
 
 int _abs(int x)
