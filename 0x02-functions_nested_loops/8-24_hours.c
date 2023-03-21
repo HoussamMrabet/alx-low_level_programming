b1778173a4a08721854b76d1be22d05b903737c3
HoussamMrabet <dodslim100@gmail.com>
2023-03-21 11:38:28 -0700

    update

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,18 +8,18 @@
 
 void jack_bauer(void)
 {
-	int i, j, k;
-	char min[2], hr[2], day[5];
+        int i, j, k;
+        char min[2], hr[2], day[5];
 
-	for (i = 0; i < 24; i++)
-		for (j = 0; j < 60; j++)
-		{
-			min = j < 10 ? '0' + j : j;
-			hr = i < 10 ? '0' + i : i;
-			day = i + ":" + j;
+        for (i = 0; i < 24; i++)
+                for (j = 0; j < 60; j++)
+                {
+                        min = j < 10 ? '0' + j : j;
+                        hr = i < 10 ? '0' + i : i;
+                        day = i + ":" + j;
 
-			for (k = 0; k <= 5; k++)
-				_putchar(day[k]);
-			_putchar('\n');
-		}	
+                        for (k = 0; k <= 5; k++)
+                                _putchar(day[k]);
+                        _putchar('\n');
+                }
 }
