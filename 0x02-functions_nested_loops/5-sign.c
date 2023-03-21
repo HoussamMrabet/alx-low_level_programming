a335c302afedef8874085e82747bee76041bb3bd
HoussamMrabet <dodslim100@gmail.com>
2023-03-21 06:44:53 -0700

    update 5-sign.c: betty style error and warning

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,8 +5,8 @@
  * Description: A function that prints the sign of a number
  * @n: number
  * Return: Returns 1 and prints + if n is greater than zero,
- 		0 and prints 0 if n is zero,
-		-1 and prints - if n is less than zero
+ * 		Returns 0 and prints 0 if n is zero,
+ * 		Returns -1 and prints - if n is less than zero
 */
 
 int print_sign(int n)
