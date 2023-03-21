a05fd98e61bfc9371aebe44e33a36015cb4f8967
HoussamMrabet <dodslim100@gmail.com>
2023-03-21 08:09:48 -0700

    update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,14 +5,12 @@
  * Description: A function that prints the last digit of a number
  * @x: an integer
  * Return: returns the last digit of @x
- */
+*/
 
 int print_last_digit(int x)
 {
-    int ld;
-
-    ld = x % 10;
-    _putchar((char)ld);
-
-    return (ld);
+int ld;
+ld = x % 10;
+_putchar((char)ld);
+return (ld);
 }
