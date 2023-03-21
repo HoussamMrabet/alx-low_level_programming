c9157ebddfb681e458180192f94e9d2c42b5f155
HoussamMrabet <dodslim100@gmail.com>
2023-03-21 11:59:16 -0700

    8. I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,6 +15,7 @@ void jack_bauer(void)
 		{
 			_putchar('0' + (hr / 10));
 			_putchar('0' + (hr % 10));
+			_putchar(':');
 			_putchar('0' + (min / 10));
 			_putchar('0' + (min % 10));
 			_putchar('\n');
