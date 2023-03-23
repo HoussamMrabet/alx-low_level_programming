deae058194aa5b0d5ef8d70450e5ceda2b53f988
Houssam Mrabet <dodslim100@gmail.com>
2023-03-23 19:18:58 +0000

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - a function that checks for uppercase characte
+ * @c: ascii code for a char
+ * Return: Returns 1 if c is uppercase otherwise returns 0
+*/
+
+int _isupper(int c)
+{
+	if (c > 65 && c < 90)
+		return (1);
+	else
+		return (0);
+}
