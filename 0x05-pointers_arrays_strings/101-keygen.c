d14bf43300cf47bb472414070e2f79a724872de1
Houssam Mrabet <dodslim100@gmail.com>
2023-03-27 06:36:22 +0000

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -7,24 +7,23 @@
 *        passwords for the program 101-crackme
 * Return: Always 0 (Success)
 */
-
 int main(void)
 {
 	int pass[100];
 	int i, sum, n;
-
+	
 	sum = 0;
 	srand(time(NULL));
 	for (i = 0; i < 100; i++)
 	{
 		pass[i] = rand() % 78;
 		sum += (pass[i] + '0');
-		printf("%d", pass[i]);
+		putchar(pass[i] + '0');
 		if ((2772 - sum) - '0' < 78)
 		{
 			n = 2772 - sum - '0';
 			sum += n;
-			printf("%d", n);
+			putchar(n + '0');
 			break;
 		}
 	}
