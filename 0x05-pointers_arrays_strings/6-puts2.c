6f8a6c2a406d00c56be30d939b7c147999a2fe92
Houssam Mrabet <dodslim100@gmail.com>
2023-03-27 06:01:43 +0000

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - A function that prints every other character of a string, starting with the first character
+ * @str: String
+ * Return: Only prints
+*/
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			printf("%c", str[i]);
+	}
+	printf("\n");
+}
