de32ba8135add3e4ce19cae240621c329dadffd2
Houssam Mrabet <dodslim100@gmail.com>
2023-03-27 04:18:04 +0000

    98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - A function that updates the value of a variable to 98
+ * @*n: A pointer of type int
+ * Return: void function 
+*/
+
+void reset_to_98(int *n)
+{
+	n = 98;
+}
