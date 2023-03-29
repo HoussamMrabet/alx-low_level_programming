497d4e1842e6bac477fc52b6c9e08fcdd4a006e0
HoussamMrabet <dodslim100@gmail.com>
2023-03-29 17:46:41 +0000

    fix issues

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -8,21 +8,21 @@
 
 void rev_string(char *n)
 {
-    int i = 0;
-    int j = 0;
-    char temp;
+	int i = 0;
+	int j = 0;
+	char temp;
 
-    while (*(n + i) != '\0')
-    {
-        i++;
-    }
-    i--;
-    for (j = 0; j < i; j++, i--)
-    {
-        temp = *(n + j);
-        *(n + j) = *(n + i);
-        *(n + i) = temp;
-    }
+	while (*(n + i) != '\0')
+	{
+		i++;
+	}
+	i--;
+	for (j = 0; j < i; j++, i--)
+	{
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
+	}
 }
 
 /**
