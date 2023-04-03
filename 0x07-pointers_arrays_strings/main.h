0fafd0e7d93b7599b3e53ea762fd5d3367ed2d76
HoussamMrabet <dodslim100@gmail.com>
2023-04-03 20:09:27 +0000

    7. The line of life is a ragged diagonal between duty and desire

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -6,3 +8,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
