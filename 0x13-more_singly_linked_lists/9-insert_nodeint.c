d08b2da7620c7917ae9394820daea18de5df1e64
HoussamMrabet <dodslim100@gmail.com>
2023-04-29 13:03:29 +0100

    fix an error in the 9th task

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,53 +1,48 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * insert_nodeint_at_index - a function that inserts a new node at
- *                           a given position
- *
- * @head: pointer to the first node of the list
- * @idx: is the index of the list where the new node should be added
- * @n: element to add to the new node
- *
- * Return: NULL if anything fails or the address of the new node
+* insert_nodeint_at_index - inserts a new node at a given position
+* @head: pointer to the beginning of linked list
+* @idx: index where to insert
+* @n: value for n
+* Return: address of new node or NULL if fails
 */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node, *current;
-	unsigned int index;
+	listint_t *new, *hold = *head;
+	unsigned int i = 0;
 
-	current = *head; /*place first node at current*/
-
-	new_node = malloc(sizeof(listint_t));
-	if ((*head == NULL && idx != 0) || new_node == NULL)
-		return (NULL);
-
-	new_node->n = n; /* add our element to the new node*/
-
-	/*iterate list to node position idx - 2*/
-	for (index = 0; head != NULL && index < idx - 1; index++)
+	if (!idx)
 	{
-		current = current->next;
-		if (current == NULL)
+		new = malloc(sizeof(listint_t));
+		if (!new)
 			return (NULL);
+
+		(*new).n = n;
+		(*new).next = *head;
+
+		*head = new;
+
+		return (*head);
 	}
 
-	if (idx == 0) /*if the index for new node is 0*/
+	while (hold)
 	{
-		/*first node will be moved to second node*/
-		new_node->next = *head;
-		/*new node will be placed as the first node*/
-		*head = new_node;
-	}
-	else if (current->next) /*if index where to add our new node is not 0*/
-	{
-		new_node->next = current->next; /*place current node after new node*/
-		current->next = new_node;/*set the new node at index idx*/
-	}
-	else /*if node position is not present in the list*/
-	{
-		new_node->next = NULL;/*set next addr as NULL, indicates last node*/
-		current->next = new_node;/*set the new node at the last position in list*/
+		if (i == (idx - 1))
+		{
+			new = malloc(sizeof(listint_t));
+			if (!new)
+				return (NULL);
+			(*new).n = n;
+			(*new).next = (*hold).next;
+			(*hold).next = new;
+			return (new);
+		}
+		hold = (*hold).next;
+		i++;
 	}
 
-	return (new_node);
+	return (NULL);
 }
